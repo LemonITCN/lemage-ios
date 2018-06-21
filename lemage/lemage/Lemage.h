4810a6bb21451a6f08f910096e3938e43c8e27f6
LemonITCN <liuri@lemonit.cn>
2018-06-21 10:27:54 +0800

    增加lemage预览器的备注

--- lemage/lemage/Lemage.h
+++ lemage/lemage/Lemage.h
@@ -95,7 +95,7 @@ typedef void (^ LEMAGE_RESULT_BLOCK)(NSArray<NSString *> *imageUrlList , BOOL is
 /**
  启动图片预览器
 
- @param imageUrlArr 要预览的图片URL数组，如果对象为nil或数组为空，那么拒绝显示图片预览器
+ @param imageUrlArr 要预览的图片URL数组，支持lemageURL和http(s)URL如果对象为nil或数组为空，那么拒绝显示图片预览器
  @param choosedImageUrlArr 已经选择的图片Url数组
  @param allowChooseCount 允许选择的图片数量，如果传<=0的数，表示关闭选择功能（选择器右上角是否有选择按钮），如果允许选择数量大于choosedImageUrlArr数组元素数量，那么会截取choosedImageUrlArr中的数组前allowChooseCount个元素作为已选择图片
  @param themeColor 主题颜色，这个颜色会作为完成按钮、选择顺序标识的背景色
